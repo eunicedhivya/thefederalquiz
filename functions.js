44877b79ff12a33f8f870de76f1f0c30caf21cc5
indhu1196 <indhumathi9900@gmail.com>
2019-07-29 16:03:51 +0530

    Radio Button

--- functions.js
+++ functions.js
@@ -39,8 +39,9 @@ function checkAnswer(){
     if(checkedOption === listOfQuestions[pos]["answer"]){
 
         // jQuery("#questionslide input:checked+label").css("color", "green")
+        
         jQuery("#questionslide input:checked").parent().css("background-color", "#c1d89f").css("color", "#23a216")
-
+        jQuery('#quiz_content input[type=radio]:checked').addClass('green')
         score++;
 
         jQuery("<p>Correct answer! To know more <a href='"+reference+"' target='_blank'>click here</a></p>" ).fadeIn("slow")
@@ -51,12 +52,14 @@ function checkAnswer(){
         // jQuery("#questionslide input:checked+label").css("color", "red");
 
         jQuery("#questionslide input:checked").parent().css("background-color", "#facdbf").css("color", "#d02417")
+        jQuery("#quiz_content input[type=radio]:checked").addClass('red')
 
         jQuery("#questionslide input").each(function(){
             
             if (jQuery(this).val() === listOfQuestions[pos]["answer"]) {
                
                 jQuery(this).parent().css("background-color", "#c1d89f").css("color", "#23a216")
+                jQuery(this).addClass('green')
                 // jQuery(this).next().css("color", "green")
 
             }
@@ -107,7 +110,7 @@ function createAQuestion() {
     // quiz.innerHTML = "<h3>" +  + "</h3>";
     // quiz.innerHTML += "<header> HEADLINE OF QUIZ GOES HERE </header>"
     quiz.innerHTML += "<div class='row clearfixsol'><img class='fade-in' src='" + leadVisual + "' /><h3>"+question+"</h3></div>"
-    quiz.innerHTML += "<div class='row clearfixsol'><div class='col col1 quizcounter'> <p>Points</p> <p><span>"+score+"</span>/"+listOfQuestions.length+"</p> </div> <div class='col col2'> <label for='choA'><input onclick='checkAnswer()' type='radio' name='choices' value='A'> " + choiceA + "</label><br> <label for='choB'><input onclick='checkAnswer()' type='radio' name='choices' value='B'> " + choiceB + "</label><br><label for='choC'> <input onclick='checkAnswer()' type='radio' name='choices' value='C'> " + choiceC + "</label><br> <label for='choC'> <input onclick='checkAnswer()' type='radio' name='choices' value='D'> " + choiceD + "</label><br> <div class='referencemsg'></div></div></div>";
+    quiz.innerHTML += "<div class='row clearfixsol'><div class='col col1 quizcounter'> <p>Points</p> <p><span>"+score+"</span>/"+listOfQuestions.length+"</p> </div> <div class='col col2'> <div class= 'ip-container'><input onclick='checkAnswer()' type='radio' name='choices' value='A' id='choA'> <label for='choA'>" + choiceA + " </label></div><br> <div class= 'ip-container'><input onclick='checkAnswer()' type='radio' name='choices' value='B' id='choB'><label for='choB'> " + choiceB + "</label></div><br><div class= 'ip-container'><input onclick='checkAnswer()' type='radio' name='choices' value='C' id='choC'><label for='choC'> " + choiceC + "</label> </div> <br> <div class='ip-container'>  <input onclick='checkAnswer()' type='radio' name='choices' value='D'> <label for='choC'>" + choiceD + "</label><br> </div><div class='referencemsg'></div></div></div>";
     // quiz.innerHTML += "<div class='row clearfixsol'><div class='col col1'> <p>Points</p> <p><span>"+score+"</span>/"+listOfQuestions.length+"</p> </div> <div class='col col2'> <input onclick='checkAnswer()' type='radio' name='choices' value='A'> <label for='choA'>" + choiceA + "</label><br> <input onclick='checkAnswer()' type='radio' name='choices' value='B'> <label for='choB'>" + choiceB + "</label><br> <input onclick='checkAnswer()' type='radio' name='choices' value='C'> <label for='choC'>" + choiceC + "</label><br> <input onclick='checkAnswer()' type='radio' name='choices' value='D'> <label for='choC'>" + choiceD + "</label><br> <div class='referencemsg'></div></div></div>";
 
     // quiz.innerHTML += "<div class='row clearfixsol footer'><p class='pull-left width50'>Q"+(pos+1)+" of "+listOfQuestions.length+"</p><button onclick='gotoNextQuestion()' class='pull-right width50 next fade-in' disabled='disabled' id='choiceSubmit'>Next &raquo;</button> </div>";
